e0660dc9d5b4e3a40624873456f04cc4cbbd97c7
Hack Reactor Students <accounts@hackreactor.com>
2017-03-10 00:13:53 -0800

    Started app construction.

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-  console.log('Express server is up on 3000');
+app.listen(4568, function(){
+  console.log('Express server is up on 4568');
 })
